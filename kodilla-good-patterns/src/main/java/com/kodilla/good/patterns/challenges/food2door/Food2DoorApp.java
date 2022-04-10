e0008369740597d0ddc1a881c5bb03883882c462
Daniel Misior <daniel.misior09@gmail.com>
2022-04-10 18:28:15 +0200

    13.4. Food2Door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Food2DoorApp.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Food2DoorApp.java
@@ -1,6 +1,7 @@
 package com.kodilla.good.patterns.challenges.food2door;
 
 public class Food2DoorApp {
+
     public static void main(String[] args) {
 
         OrderRetriever orderRetriever = new OrderRetriever();
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/OrderRetriever.java
@@ -10,7 +10,6 @@ public class OrderRetriever {
         Map<String, Integer> order1 = new HashMap<>();
         order1.put("Water 1L", 55);
         order1.put("Garlic", 2);
-        order1.put("Sunflower seeds", 5);
 
         return new Order(order1, "Konopnicka 10, 50-452 Wroclaw");
     }
