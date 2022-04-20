8ccbec86c4015643b23bd19efb44de306dbaf396
Daniel Misior <daniel.misior09@gmail.com>
2022-04-20 16:02:15 +0200

    14.2. Calculator

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -21,14 +21,28 @@ public class CalculatorTestSuite {
             double subResult = calculator.sub(10, 20);
             double mulResult = calculator.mul(10, 20);
             double divResult = calculator.div(10, 20);
-            double exceptionExpected = calculator.div(10, 0);
 
             //Then
             assertEquals(addResult, 30);
             assertEquals(subResult, -10);
             assertEquals(mulResult, 200);
             assertEquals(divResult, 0.5);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot be divided by 0");
+        }
+    }
+
+    @Test
+    public void testWhenDividedBy0() {
+        try {
+            //Given
+            //When
+            double exceptionExpected = calculator.div(10, 0);
+
+            //Then
             assertEquals(exceptionExpected, "Cannot be divided by 0");
+
         } catch (ArithmeticException e) {
             System.out.println("Cannot be divided by 0");
         }
