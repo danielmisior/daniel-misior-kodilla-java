45f0efa4331e0f60d2dbbc05162a024c7c0f7fb6
Daniel Misior <daniel.misior09@gmail.com>
2022-04-20 17:19:24 +0200

    15.1. Logger

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,2 +1,20 @@
-package com.kodilla.patterns.singleton;public class LoggerTestSuite {
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    @Test
+    void testGetLastLog() {
+        //Given
+        Logger.getInstance().log("Last log");
+
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+
+        //Then
+        assertEquals("Last log", lastLog);
+    }
 }
